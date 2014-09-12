c671b7081f42875a18ef0ca274a1190f2d764e81
Christopher St. John <chrisjstjohn@gmail.com>
2014-09-12 08:11:08 -0400

    Program to generate sets of assets for different Android screen sizes [#78299002]

---
+++ TileCreator/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TileCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Make Tiles";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(848, 240);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 293);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ TileCreator/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileCreator
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Size> screenSizes = new Dictionary<string, Size>{
+            {"ldpi", new Size(240, 320)},
+            {"mdpi", new Size(320, 470)},
+            {"hdpi", new Size(720, 960)},
+            {"xhdpi", new Size(1440, 1920)}
+        };
+
+        Dictionary<string, Size> tileSizes = new Dictionary<string, Size>();
+        Dictionary<string, Size> shapeSizes = new Dictionary<string, Size>();
+
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (string key in screenSizes.Keys)
+            {
+                int maxTileWidth = (int)(screenSizes[key].Width / 3 * .9);
+                int maxTileHeight = (int)(screenSizes[key].Height / 3 * .9);
+
+                // standard playing cards have width 2.5 in, height 3.5 in
+                // we want the same ratio
+
+                int tileWidth, tileHeight;
+
+                tileWidth = maxTileWidth;
+                tileHeight = (int)(maxTileWidth * 3.5 / 2.5);
+
+                double scale = maxTileHeight / (maxTileWidth * 3.5 / 2.5);
+                if (scale < 1.0)
+                {
+                    tileWidth = (int)(scale * tileWidth);
+                    tileHeight = (int)(scale * tileHeight);
+                }
+
+                tileSizes[key] = new Size(tileWidth, tileHeight);
+
+                shapeSizes[key] = new Size((int)(tileWidth * .9), (int)(tileHeight * .9 / 3));
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Bitmap GetShape(Size size, int iColor, int iShape, int iShading)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            Size s = size;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            Color color = new Color[] { Color.Red, Color.Green, Color.Purple }[iColor - 1];
+            Pen pen = new Pen(new SolidBrush(color), Math.Max(2f, .02f * size.Height));
+            Region shapeRegion;
+            GraphicsPath shapeBorder;
+
+            if (iShading == 1)
+            {
+                // filled
+                g.FillRectangle(new SolidBrush(color), new Rectangle(new Point(0, 0), s));
+            }
+            else if (iShading == 2)
+            {
+                Pen thin = new Pen(new SolidBrush(color), 1f);
+                // lined
+                for (int i = 10; i < Math.Max((s.Width - 1), (s.Height - 1)) * 2; i += 5)
+                {
+                    g.DrawLine(thin, new Point(i, 0), new Point(0, i));
+                }
+            }
+            else
+            {
+                // no fill
+            }
+
+            Region complementRegion = new Region(new RectangleF(s.Width * -.1f, s.Height * -.1f, s.Width, s.Height));
+
+
+            g.TranslateTransform(s.Width * .1f, s.Height * .1f);
+            s = new Size((int)(size.Width * .8), (int)(size.Height * .8));
+
+
+            #region Create Shapes as Regions
+            if (iShape == 1)
+            {
+                // rounded rectangle
+                shapeBorder = new GraphicsPath();
+                shapeBorder.AddClosedCurve(new Point[]{
+                    new Point(0, 0),
+                    new Point((s.Width - 1), 0),
+                    new Point((s.Width - 1), (s.Height - 1)),
+                    new Point(0, (s.Height - 1))
+                }, .2f);
+                shapeRegion = new Region(shapeBorder);
+
+            }
+            else if (iShape == 2)
+            {
+                // Diamond
+                shapeBorder = new GraphicsPath();
+                shapeBorder.AddPolygon(new Point[]{
+                    new Point((s.Width - 1) / 2, 0), // top
+                    new Point(0, (s.Height - 1) / 2), // left
+                    new Point((s.Width - 1)/2, (s.Height - 1)), // bottom
+                    new Point((s.Width - 1), (s.Height - 1) / 2) // right
+                });
+                shapeRegion = new Region(shapeBorder);
+            }
+            else
+            {
+                // squiggle
+                shapeBorder = new GraphicsPath();
+                shapeBorder.AddClosedCurve(new Point[]{
+                    new Point((s.Width - 1), 0),
+                    new Point((s.Width - 1), (s.Height - 1)/2),
+                    new Point(2*(s.Width - 1)/3, (s.Height - 1)),
+                    new Point((s.Width - 1)/3, 2*(s.Height - 1)/3),
+                    new Point(0, (s.Height - 1)),
+                    new Point(0, (s.Height - 1)/2),
+                    new Point((s.Width - 1)/3, 0),
+                    new Point(2*(s.Width - 1)/3, (s.Height - 1)/3)
+                });
+                shapeRegion = new Region(shapeBorder);
+            }
+            #endregion
+
+            shapeRegion.Complement(complementRegion);
+
+            g.FillRegion(Brushes.White, shapeRegion);
+            g.DrawPath(pen, shapeBorder);
+            return bmp;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int offset = 0;
+            foreach (string key in shapeSizes.Keys)
+            {
+                Bitmap shape = GetShape(shapeSizes[key], random.Next(3) + 1, random.Next(3) + 1, random.Next(3) + 1);
+                e.Graphics.DrawImage(shape, new Point(offset, 20));
+                offset += shapeSizes[key].Width + 20;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+
+            if (folderBrowserDialog1.ShowDialog(this) == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            string folder = folderBrowserDialog1.SelectedPath;
+
+            foreach (string key in tileSizes.Keys)
+            {
+                Size tileSize = tileSizes[key];
+                Size shapeSize = shapeSizes[key];
+                string keyPath = Path.Combine(folder, key);
+                Directory.CreateDirectory(keyPath);
+
+
+                int betweenShapes = (int)(0.05 * shapeSize.Height);
+                int leftSpacing = (tileSize.Width - shapeSize.Width) / 2;
+
+                for (int shape = 1; shape < 4; shape++)
+                {
+                    for (int color = 1; color < 4; color++)
+                    {
+                        for (int shading = 1; shading < 4; shading++)
+                        {
+                            Bitmap shapeImage = GetShape(shapeSize, color, shape, shading);
+                            for (int quantity = 1; quantity < 4; quantity++)
+                            {
+                                Bitmap tileImage = new Bitmap(tileSize.Width, tileSize.Height);
+                                Graphics tileGraphics = Graphics.FromImage(tileImage);
+                                tileGraphics.FillRectangle(Brushes.White, new Rectangle(0, 0, tileSize.Width, tileSize.Height));
+
+                                int topSpacing = (tileSize.Height - shapeSize.Height * quantity - betweenShapes * (quantity - 1)) / 2;
+                                Point[] shapeLocations = new Point[quantity];
+                                shapeLocations[0] = new Point(leftSpacing, topSpacing);
+                                for (int i = 1; i < quantity; i++)
+                                {
+                                    shapeLocations[i] = new Point(leftSpacing, shapeLocations[i - 1].Y + shapeSize.Height + betweenShapes);
+                                }
+
+                                for (int shapeIndex = 0; shapeIndex < quantity; shapeIndex++)
+                                {
+                                    tileGraphics.DrawImage(shapeImage, shapeLocations[shapeIndex]);
+                                }
+
+                                tileImage.Save(Path.Combine(keyPath, string.Format("{0}{1}{2}{3}.jpg", quantity, shape, color, shading)), ImageFormat.Jpeg);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
